8ca8302076ced9fbb59ac3da813b26f04c34b253
tranguv <89886131+tranguv@users.noreply.github.com>
2023-03-02 22:48:37 -0500

    Initial commit

---
+++ myScript.js
@@ -0,0 +1,10 @@
+//function to perfom scrolling of the left arrow
+function leftScroll() {
+    const left = document.querySelector(".scrollingPJ");
+    left.scrollBy(-500, 0);
+  }
+
+function rightScroll() {
+    const right = document.querySelector(".scrollingPJ");
+    right.scrollBy(500, 0);
+}
